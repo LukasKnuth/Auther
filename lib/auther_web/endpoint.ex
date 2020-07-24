0fd1473031c269f094374ed01f2c6b17e7014f44
Lukas Knuth <mr.luke.187@googlemail.com>
2020-07-24 11:20:48 +0200

    Added basic Pow setup.

--- lib/auther_web/endpoint.ex
+++ lib/auther_web/endpoint.ex
@@ -50,5 +50,6 @@ defmodule AutherWeb.Endpoint do
   plug Plug.MethodOverride
   plug Plug.Head
   plug Plug.Session, @session_options
+  plug Pow.Plug.Session, otp_app: :auther
   plug AutherWeb.Router
 end
