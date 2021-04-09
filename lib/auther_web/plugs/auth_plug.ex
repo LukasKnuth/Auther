1e52b15db42a96624e2154400609ee8654392ff9
Lukas Knuth <lukas.knuth@betterdoc.de>
2021-04-09 23:35:28 +0200

    Added authentication plug to ensure login for applied scopes.

---
+++ lib/auther_web/plugs/auth_plug.ex
@@ -0,0 +1,28 @@
+defmodule AutherWeb.AuthPlug do
+  alias AutherWeb.Router.Helpers, as: Routes
+  alias AutherWeb.Session
+  alias Plug.Conn
+
+  import Conn, only: [halt: 1]
+  import Phoenix.Controller, only: [redirect: 2]
+
+  @behaviour Plug
+
+  @impl Plug
+  def init(_opts), do: []
+
+  @impl Plug
+  def call(conn, _opts) do
+    conn
+    |> Session.is_signed_in?()
+    |> auth_reply(conn)
+  end
+
+  defp auth_reply(true, conn), do: Session.restore(conn)
+
+  defp auth_reply(false, conn) do
+    conn
+    |> redirect(to: Routes.session_path(conn, :form))
+    |> halt()
+  end
+end
---
+++ test/auther_web/plugs/auth_plug_test.exs
@@ -0,0 +1,42 @@
+defmodule AutherWeb.AuthPlugTest do
+  use AutherWeb.ConnCase
+
+  alias Auther.Accounts
+  alias Auther.Accounts.User
+  alias AutherWeb.Session
+
+  test "redirects and halts if user isn't logged in" do
+    conn = run_plug_with()
+
+    assert conn.halted == true
+    assert redirected_to(conn) == AutherWeb.Router.Helpers.session_path(conn, :form)
+  end
+
+  test "restores the sessio if user is logged in" do
+    conn = run_plug_with(user_fixture())
+
+    assert conn.halted == false
+    assert Session.is_signed_in?(conn) == true
+    assert %User{} = Session.current_user!(conn)
+  end
+
+  defp user_fixture do
+    Mox.stub(Auther.Security.Password.Mock, :hash, fn _ -> "pretend_like_im_hashed" end)
+    {:ok, user} = Accounts.create_user(%{name: "test", email: "test@user.de", password: "a", password_confirmation: "a"})
+    user
+  end
+
+  defp run_plug_with(user \\ nil) do
+    build_conn()
+    |> bypass_through(AutherWeb.Router, :browser)
+    |> get("/")
+    |> maybe_put_session(user)
+    |> AutherWeb.AuthPlug.call(
+      AutherWeb.AuthPlug.init([])
+    )
+  end
+
+  defp maybe_put_session(conn, nil), do: conn
+
+  defp maybe_put_session(conn, %User{} = user), do: Session.sign_in(conn, user)
+end
