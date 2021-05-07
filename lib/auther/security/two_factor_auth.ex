22d85b90327a3948d672fddbf9922d722a9b0593
Lukas Knuth <lukas.knuth@betterdoc.de>
2021-05-07 16:15:58 +0200

    Added otpauth_uri method to 2FA module.

--- lib/auther/security/two_factor_auth.ex
+++ lib/auther/security/two_factor_auth.ex
@@ -1,16 +1,22 @@
 defmodule Auther.Security.TwoFactorAuth do
   use Knigge, otp_app: :auther
 
+  alias Auther.Accounts.User
+
   @moduledoc """
   Allows setting up and using a means of authenticating a user by a secondary factor (2FA) through a one-time password.
   """
 
   @type pre_shared_secret :: String.t()
   @type one_time_password :: String.t()
+  @type otpauth_uri :: String.t()
 
   @doc "Generates a secret to be shared between Auther and the user to generate one-time passwords from"
   @callback generate_secret() :: pre_shared_secret()
 
   @doc "Checks if the given one-time password is valid for the pre-shared secret"
   @callback valid?(pre_shared_secret(), one_time_password()) :: boolean()
+
+  @doc "Creates a otpauth:// schema URI for easy setup via QRCodes"
+  @callback otpauth_uri(pre_shared_secret(), User.t()) :: otpauth_uri()
 end
--- lib/auther/security/two_factor_auth/totp.ex
+++ lib/auther/security/two_factor_auth/totp.ex
@@ -1,6 +1,10 @@
 defmodule Auther.Security.TwoFactorAuth.TOTP do
   @behaviour Auther.Security.TwoFactorAuth
 
+  alias Auther.Accounts.User
+
+  @tfa_issuer "Auther"
+
   @impl true
   def generate_secret do
     NimbleTOTP.secret()
@@ -12,6 +16,19 @@ defmodule Auther.Security.TwoFactorAuth.TOTP do
     secret
     |> decode_secret()
     |> NimbleTOTP.valid?(otp)
+  end
+
+  @impl true
+  def otpauth_uri(secret, user) do
+    # According to https://github.com/google/google-authenticator/wiki/Key-Uri-Format
+    user
+    |> account_identifier()
+    |> NimbleTOTP.otpauth_uri(decode_secret(secret), issuer: @tfa_issuer)
+  end
+
+  defp account_identifier(%User{email: email}) do
+    # todo sure to use email here?
+    URI.encode("#{@tfa_issuer}:#{email}")
   end
 
   defp encode_secret(secret), do: Base.encode32(secret)
